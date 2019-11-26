7e34c596723312d06498ac6c56bd87eec896ce4f
Samim <samimaftabahmed@gmail.com>
2019-11-26 23:44:27 +0530

    reducer logic

--- src/main/java/com/samhad/FileReducer.java
+++ src/main/java/com/samhad/FileReducer.java
@@ -1,6 +1,22 @@
 package com.samhad;
 
-public class FileReducer {
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
 
+import java.io.IOException;
 
+public class FileReducer extends Reducer<Text, Text, Text, Text> {
+
+    @Override
+    protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+
+        Text imageTextPath = null;
+
+        for (Text filePath : values) {
+            imageTextPath = filePath;
+            break;
+        }
+
+        context.write(imageTextPath, key);
+    }
 }
